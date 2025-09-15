3a22716aaf5ef36e83ae327df973d6c124e25169
Felix Fleku <flekufelix@gmail.com>
2025-09-15 18:01:43 +0000

    review code

--- index.ts
+++ index.ts
@@ -1,12 +1,25 @@
-
-import { generateText } from "ai";
-//import the google module from the ai-sdk package
+import { stepCountIs, streamText } from "ai";
 import { google } from "@ai-sdk/google";
+import { SYSTEM_PROMPT } from "./prompts";
+import { getFileChangesInDirectoryTool } from "./tools";
 
-//specify the model to use for generating text and a prompt
-const { text } = await generateText({
+const codeReviewAgent = async (prompt: string) => {
+  const result = streamText({
     model: google("models/gemini-2.5-flash"),
-    prompt: " What will the future of AI will be like and what should we do to level  up?",
-})
+    prompt,
+    system: SYSTEM_PROMPT,
+    tools: {
+      getFileChangesInDirectoryTool: getFileChangesInDirectoryTool,
+    },
+    stopWhen: stepCountIs(10),
+  });
 
-console.log(text);
+  for await (const chunk of result.textStream) {
+    process.stdout.write(chunk);
+  }
+};
+
+// Specify which directory the code review agent should review changes in your prompt
+await codeReviewAgent(
+  "Review the code changes in '../my-agent' directory, make your reviews and suggestions file by file",
+);
